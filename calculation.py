12416a53e1d4f43a83e178421a7784e69b02d0e9
TarakaKoda <taraka.koda@gmail.com>
2023-05-01 17:51:16 +0530

    calculations

---
+++ calculation.py
@@ -0,0 +1,21 @@
+def add(a: int | float, b: int | float) -> int | float:
+    """This function takes arguments of two numbers: int or float, and returns the addition of two numbers"""
+    return a + b
+
+
+def sub(a: int | float, b: int | float) -> int | float:
+    """This function takes arguments of two numbers: int or float, and return the subtraction of two numbers"""
+    return a - b
+
+
+def multiplication(a: int | float, b: int | float) -> int | float:
+    """This function takes arguments of two numbers: int or float, and return the multiplication of two numbers"""
+    return a * b
+
+
+def division(a: int | float, b: int | float) -> int | float:
+    """This function takes arguments of two numbers: int or float, and return the division of two numbers"""
+    return a / b
+
+
+
